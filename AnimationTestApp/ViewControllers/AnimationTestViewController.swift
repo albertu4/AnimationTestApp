ceda3063c3e2086618fe537a8270ac43ef784f31
Miwka <97275239+albertu4@users.noreply.github.com>
2022-10-10 22:10:49 +0800

    Code review

--- AnimationTestApp/ViewControllers/AnimationTestViewController.swift
+++ AnimationTestApp/ViewControllers/AnimationTestViewController.swift
@@ -9,50 +9,57 @@ import Spring
 
 class AnimationTestViewController: UIViewController {
     
-    private var animation = Animation.getAnimation()
-
     @IBOutlet var animationView: SpringView!
-    
-    @IBOutlet var presetLabel: UILabel!
-    @IBOutlet var curveLabel: UILabel!
-    @IBOutlet var forceLabel: UILabel!
-    @IBOutlet var durationLabel: UILabel!
-    @IBOutlet var delayLabel: UILabel!
-    
     @IBOutlet var animationStartButton: SpringButton!
+    
+    @IBOutlet var animationSettingLabels: [UILabel]!
+    
+    private var animation = Animation.getAnimation()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         animationView.layer.cornerRadius = 10
         animationStartButton.layer.cornerRadius = 10
         
-        animationStartButton.setTitle(animation.preset, for: .normal)
+        animationStartButton.setTitle("Run \(animation.preset)", for: .normal)
+        changeLabelsText()
     }
 
-    @IBAction func startAnimationButton(_ sender: SpringButton) {
+    @IBAction func startAnimation(_ sender: SpringButton) {
+        changeAnimationSettings()
+        animationView.animate()
+        
+        animation = Animation.getAnimation()
+        changeLabelsText()
+        
+        sender.animation = "pop"
+        sender.setTitle("Run \(animation.preset)", for: .normal)
+        sender.animate()
+    }
+    
+    private func changeAnimationSettings() {
         animationView.animation = animation.preset
         animationView.curve = animation.curve
         animationView.force = CGFloat(animation.force)
         animationView.duration = CGFloat(animation.duration)
         animationView.delay = CGFloat(animation.delay)
-        changeLabelText()
-        animationView.animate()
-        
-        animation = Animation.getAnimation()
-        
-        sender.animation = "pop"
-        sender.setTitle(animation.preset, for: .normal)
-        sender.animate()
-        
-        
     }
     
-    private func changeLabelText() {
-        presetLabel.text = "Preset: \(animation.preset)"
-        curveLabel.text = "Curve: \(animation.curve)"
-        forceLabel.text = "Force: \(String(format: "%.2f", animation.force))"
-        durationLabel.text = "Duration: \(String(format: "%.2f", animation.duration))"
-        delayLabel.text = "Delay: \(String(format: "%.2f", animation.duration))"
+    private func changeLabelsText() {
+        animationSettingLabels.forEach {
+            switch $0.tag {
+            case 0:
+                $0.text = "Preset: \(animation.preset)"
+            case 1:
+                $0.text = "Curve: \(animation.curve)"
+            case 2:
+                $0.text = "Force: \(String(format: "%.2f", animation.force))"
+            case 3:
+                $0.text = "Duration: \(String(format: "%.2f", animation.duration))"
+            default:
+                $0.text = "Delay: \(String(format: "%.2f", animation.duration))"
+            }
+        }
     }
 }
 
